b3f7ad064638e9fdd2e2da57363380bb5e951aa8
Andy Richardson <axrich@gmail.com>
2012-12-31 19:13:11 -0600

    more styling

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,7 @@ from posts.models import Post
 from django.contrib import admin
 
 class PostAdmin(admin.ModelAdmin):
-	fields = ['title', 'caption', 'tags', 'proj_loc', 'url', 'pub_date', 'content']
+	fields = ['title', 'caption', 'tags', 'header_css', 'header', 'proj_loc', 'url', 'github', 'pub_date', 'content']
 	list_display = ('title', 'pub_date')
 	list_filter = ['pub_date']
 
--- posts/models.py
+++ posts/models.py
@@ -4,13 +4,16 @@ from tagging.models import Tag
 from ckeditor.fields import RichTextField
 
 class Post(models.Model):
-	title = models.CharField(max_length=50)
-	caption = models.CharField(max_length=200)
-	proj_loc = models.CharField(max_length=50)
-	url = models.SlugField()
-	pub_date = models.DateTimeField('date published')
-	tags = TagField()
-	content = RichTextField(config_name='content_ckeditor')
+	title 		= models.CharField(max_length=50)
+	caption 	= models.CharField(max_length=200)
+	header_css 	= models.TextField()
+	header 		= models.CharField(max_length=50)
+	proj_loc 	= models.CharField(max_length=50)
+	url 		= models.SlugField()
+	github		= models.CharField(max_length=200)
+	pub_date 	= models.DateTimeField('date published')
+	tags 		= TagField()
+	content 	= RichTextField(config_name='content_ckeditor')
 
 	def get_tags(self):
 		return Tag.objects.get_for_object(self)
